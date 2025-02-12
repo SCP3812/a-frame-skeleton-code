ce9333948b31432305399a252bead52696759086
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2025-02-12 17:28:45 +0000

    Joe Biden

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const snow = document.getElementById("snow")
+let xPos = -5;
+let yPos = 0;
 
+function moveSnow() {
+    if (xPos < 5) {
+        snow.xPos+=0.01
+        snow.yPos+=0.01
+    }
+
+    else {
+        snow.xPos+=-5
+        snow.yPos+=0
+    }
+}
+
+moveSnow()
